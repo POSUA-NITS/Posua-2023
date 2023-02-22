4647c6a9d28701f412e52ed2ae2015226029def8
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-02-22 21:10:12 +0530

    ADDED: Router and folder structure

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/events'>Events</Link></li>
+        <li><Link to='/gallery'>Gallery</Link></li>
+        <li><Link to='/teams'>Teams</Link></li>
+      </ul>
+    </nav>
+  )
+};
+
+export default Navbar;
