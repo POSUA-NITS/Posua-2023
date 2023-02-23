9a74f3dfee74ef7b6f9e9b9b617bcc9fde27afe5
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-02-23 18:02:57 +0530

    updated team landing

--- src/Pages/Teams/Teams.jsx
+++ src/Pages/Teams/Teams.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-
 import TeamPosuaWording from "../../Components/Team/TeamPosuaWording/TeamPosuaWording";
 
 const Teams = () => {
   return (
-    
     <div className="containerTeam">
-        <TeamPosuaWording />
+      <TeamPosuaWording />
     </div>
-    
-  )
+  );
 };
 
 export default Teams;
