4ab4856c595f9682fe5d45262e2fee3dc3cbb477
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-02-23 19:23:46 +0530

    Minor prettier changes

--- src/Components/Team/TeamMembers/TeamMemberStructure.jsx
+++ src/Components/Team/TeamMembers/TeamMemberStructure.jsx
@@ -1,41 +1,41 @@
 import Lottie from "react-lottie";
-import animationData from '../../../Assets/Pepa.json';
+import animationData from "../../../Assets/Pepa.json";
 import styles from "./TeamMemStructure.module.scss";
-import Gamusa from '../../../Assets/Gamusa.png';
+import Gamusa from "../../../Assets/Gamusa.png";
 
 const TeamMemberStructure = (data) => {
-    
-    const defaultOptionsPepa = {
-        loop: true,
-        autoplay: false,
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-    };
+  const defaultOptionsPepa = {
+    loop: true,
+    autoplay: false,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-    return (
-        <div className={styles.containerMemStructure}>
-            <div className={styles.teamMemberGrid}>
-                <div className={styles.imageHoldingWrapper}>
-                    <div className={styles.emptyColoredDiv}>
-                        <img src={data.image}/>
-                    </div>
-                </div>
-                <Lottie className={styles.LottiesSVG}
-                    options={defaultOptionsPepa}
-                    height={400}
-                    width={400}
-                />
-                <div className={styles.gamusaWriting}>
-                    <img src={Gamusa}/>
-                    <p>{data.name}</p>
-                    <p>{data.desig}</p>
-                    <p>{data.email}</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.containerMemStructure}>
+      <div className={styles.teamMemberGrid}>
+        <div className={styles.imageHoldingWrapper}>
+          <div className={styles.emptyColoredDiv}>
+            <img src={data.image} />
+          </div>
         </div>
-    )
-}
+        <Lottie
+          className={styles.LottiesSVG}
+          options={defaultOptionsPepa}
+          height={400}
+          width={400}
+        />
+        <div className={styles.gamusaWriting}>
+          <img src={Gamusa} />
+          <p>{data.name}</p>
+          <p>{data.desig}</p>
+          <p>{data.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TeamMemberStructure
+export default TeamMemberStructure;
