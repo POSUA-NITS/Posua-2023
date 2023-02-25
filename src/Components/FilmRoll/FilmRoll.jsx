c40020146a3965a51fc702f1e08183e151b3ddf0
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-02-25 19:15:59 +0530

    UPDATE: film-roll

---
+++ src/Components/FilmRoll/FilmRoll.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './FilmRoll.module.scss'
+
+const FilmRoll = () => {
+  return (
+    <div className={Styles.enclose}>
+      
+    </div>
+  )
+}
+
+export default FilmRoll
