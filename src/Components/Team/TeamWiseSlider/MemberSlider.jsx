0454589fa591b41cd43b6f9d603452487c56fd50
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-02-25 18:08:57 +0530

    ADD - ImageSlider in Team page

---
+++ src/Components/Team/TeamWiseSlider/MemberSlider.jsx
@@ -0,0 +1,35 @@
+import Dummy from "../../../Assets/texture.png";
+import Card from "../../../Assets/card.png";
+
+const MemberSlider = [
+  {
+    id: 1,
+    card: `${Card}`,
+    src: `${Dummy}`,
+    text: "Badminton",
+    alt: "Badminton Image",
+  },
+  {
+    id: 2,
+    card: `${Card}`,
+    text: "Chess",
+    src: `${Dummy}`,
+    alt: "Chess Image",
+  },
+  {
+    id: 3,
+    card: `${Card}`,
+    text: "Table tennis",
+    src: `${Dummy}`,
+    alt: "Table Tennis Image",
+  },
+  {
+    id: 4,
+    card: `${Card}`,
+    text: "ok",
+    src: `${Dummy}`,
+    alt: "Table Tennis Image",
+  },
+];
+
+export default MemberSlider;
