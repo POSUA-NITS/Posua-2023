a0bce31c639ef8f58483cc331f6b94e701e3f2af
kherkatary <kherkatarysan456@gmail.com>
2023-02-27 17:09:32 +0530

    event carousal added, and phool animation corrected, phool position yet to be decided

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -2,16 +2,24 @@ import data from "./data.json";
 import styles from "./Events.module.scss";
 import { Header } from "../../Components/Header/Header";
 import { Phool } from "../../Components/Phool/Phool";
-import { Carousel } from "../../Components";
+import { Carousel, Navbar } from "../../Components";
 
 const Events = () => {
   return (
-    <div className={styles.main}>
-      <Header title="EVENTS" />
-      <div className={styles.content}>{data.content}</div>
-      <Phool />
-      <Carousel />
-    </div>
+    <>
+      <Navbar />
+
+      <div className={styles.main}>
+        <Header title="EVENTS"/>
+        <div className={styles.content}>{data.content}</div>
+        <Phool />
+        <Carousel />
+      </div>
+
+
+    </>
+
+
   );
 };
 
