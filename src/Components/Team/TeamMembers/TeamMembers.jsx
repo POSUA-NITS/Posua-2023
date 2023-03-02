426941a1865654ca4c4eb1b1fcdf805a78e2cd0b
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-03-03 01:52:22 +0530

    Update: updated images in team page

--- src/Components/Team/TeamMembers/TeamMembers.jsx
+++ src/Components/Team/TeamMembers/TeamMembers.jsx
@@ -1,44 +1,78 @@
-import Dummy from "../../../Assets/Dummy.png";
+// import Dummy from "../../../Assets/Dummy.png";
 import TeamMemberStructure from "./TeamMemberStructure";
 import styles from "./TeamMember.module.scss";
 
 const TeamMembers = () => {
   const Members = [
     {
-      name: "Dr. Pranjit Barman",
-      desig: "Mentor",
-      email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      name: "Dr. Rajat Gupta",
+      desig: "Chief Patron",
+      email: "rgupta@mech.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677783990/rajat_zptvuj.jpg",
     },
     {
       name: "Dr. Pranjit Barman",
       desig: "Mentor",
       email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677785003/Pranjit_Barman_zxorco.jpg",
     },
     {
-      name: "Dr. Pranjit Barman",
-      desig: "Mentor",
-      email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      name: "Dr. LC Saikia",
+      desig: "President",
+      email: "lcsaikia.yahoo.com",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677783991/LC_Saikia_vgwgce.jpg",
     },
     {
-      name: "Dr. Pranjit Barman",
-      desig: "Mentor",
-      email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      name: "Dr. Nabanita Adhikary",
+      desig: "Vice President",
+      email: "nabanita.ee.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677786101/Nabanita_Adhikary_cw5kjd.jpg",
     },
     {
-      name: "Dr. Pranjit Barman",
-      desig: "Mentor",
+      name: "Dr. Jyotirmoy Haloi",
+      desig: "Vice President",
       email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677784961/Jyotirmoy_Haloi_hoyyhk.jpg",
     },
     {
-      name: "Dr. Pranjit Barman",
-      desig: "Mentor",
+      name: "Manash Pratim Goswami",
+      desig: "Chief Coorinator",
       email: "pranjit@che.nits.ac.in",
-      image: `${Dummy}`,
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677787158/Manash_Goswami_xuc901.jpg",
+    },
+    {
+      name: "Afsana Yasmin",
+      desig: "Chief Coorinator",
+      email: "pranjit@che.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677787158/Afsana_Yasmin_qfdkuc.jpg",
+    },
+    {
+      name: "Manash Pratim Goswami",
+      desig: "Chief Coorinator",
+      email: "pranjit@che.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677787158/Manash_Goswami_xuc901.jpg",
+    },
+    {
+      name: "Manash Pratim Goswami",
+      desig: "Chief Coorinator",
+      email: "pranjit@che.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677787158/Manash_Goswami_xuc901.jpg",
+    },
+    {
+      name: "Manash Pratim Goswami",
+      desig: "Chief Coorinator",
+      email: "pranjit@che.nits.ac.in",
+      image:
+        "https://res.cloudinary.com/dldebeast/image/upload/v1677787158/Manash_Goswami_xuc901.jpg",
     },
   ];
 
