76082206e4face0ecd4f0f9e0cadb20131c78564
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-03-02 22:43:45 +0530

    RESOLVED: merge conflicts

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,12 +3,8 @@ import { useState } from "react";
 import Styles from "./Hero.module.scss";
 
 const Hero = () => {
-<<<<<<< HEAD
   const [winWidth, setWinWidth] = useState(window.innerWidth)
   const [winHeight, setWinHeight] = useState(window.innerHeight)
-=======
-  const [winWidth, setWinWidth] = useState(window.innerWidth);
->>>>>>> 302bff5abd3e8e06d0d8a9078fe1a1df59813d88
 
   document.getElementsByClassName("sky");
   document.getElementsByClassName("clouds");
@@ -51,7 +47,6 @@ const Hero = () => {
         {/* <img src="https://ik.imagekit.io/lbrdi6gxa/posua2023/posua-min.png?ik-sdk-version=javascript-1.4.3&updatedAt=1677767129104" alt="" className={Styles.posua} /> */}
 
         {/* </div> */}
-<<<<<<< HEAD
         <img src="/assets/Hero/hills.png" alt="" className={Styles.hills} />
         <img src="/assets/Hero/pines.png" alt="" className={Styles.pines} />
         <img src="/assets/Hero/whitish-bush.png" alt="" className={Styles.whitishBush} />
@@ -64,59 +59,6 @@ const Hero = () => {
       <div className={winWidth > 800 && winHeight<winWidth ? Styles.bush : Styles.bush_mobile}>
         <img src="/assets/Hero/bush.png" alt=""  />
       </div>
-=======
-        {/* <img src="/assets/Hero/hills.png" alt="" className={Styles.hills} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768672/posua/hills-min.png_vgj2d2.png"
-          alt=""
-          className={Styles.hills}
-        />
-        {/* <img src="https://ik.imagekit.io/lbrdi6gxa/posua2023/hills-min.png?ik-sdk-version=javascript-1.4.3&updatedAt=1677767285402" alt="" className={Styles.hills} /> */}
-        {/* <img src="/assets/Hero/pines.png" alt="" className={Styles.pines} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768721/posua/Untitled_Artwork-8_Compressed-min.png_alzhch.png"
-          alt=""
-          className={Styles.pines}
-        />
-        {/* <img src="/assets/Hero/whitish-bush.png" alt="" className={Styles.whitishBush} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768790/posua/Untitled_Artwork-10_Compressed-min.png_alqhqv.png"
-          alt=""
-          className={Styles.whitishBush}
-        />
-        {/* <img src="/assets/Hero/layer4.png" alt="" className={Styles.layer4} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768835/posua/layer4-min.png_jztyxr.png"
-          alt=""
-          className={Styles.layer4}
-        />
-        {/* <img src="/assets/Hero/layer3.png" alt="" className={Styles.layer3} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768890/posua/layer3-min.png_mq22bj.png"
-          alt=""
-          className={Styles.layer3}
-        />
-        {/* <img src="/assets/Hero/layer2.png" alt="" className={Styles.layer2} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768946/posua/layer2-min.png_w8sruq.png"
-          alt=""
-          className={Styles.layer2}
-        />
-        {/* <img src="/assets/Hero/layer1.png" alt="" className={Styles.layer1} /> */}
-        <img
-          src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768991/posua/layer1-min.png_zu6lyb.png"
-          alt=""
-          className={Styles.layer1}
-        />
-        {/* <img src="/assets/Hero/bush.png" alt="" className={Styles.bushInverted} /> */}
-      </div>
-      <img
-        // src="/assets/Hero/bush.png"
-        src="https://res.cloudinary.com/dp92qug2f/image/upload/v1677768382/posua/bush-min_dqh93w.png"
-        alt=""
-        className={winWidth > 800 ? Styles.bush : Styles.bush_mobile}
-      />
->>>>>>> 302bff5abd3e8e06d0d8a9078fe1a1df59813d88
     </>
   );
 };
