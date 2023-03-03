cbb59028b6beec9770b5ccfd2db15f3e7cbca44f
Pragya Annesha Baruah <pragya.annesha@gmail.com>
2023-03-03 18:54:21 +0530

    UPDATE: position

---
+++ src/Components/FilmRoll/Clicked.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Styles from "./FilmRoll.module.scss";
+
+const Clicked = (props) => {
+  const { clicked, setClicked } = props;
+
+  return (
+    <>
+      <div
+        className={Styles.backdrop}
+        onClick={() => {
+          setClicked("default");
+        }}
+      ></div>
+      <div className={Styles.clicked}>
+        <img src={clicked} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Clicked;
---
+++ src/Pages/Artists/UpcomingModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Data from "../../Data/UpcomingArtists.json";
+import style from "./Artists.module.scss";
+function UpcomingModal({ dataid }) {
+  let content = "",
+    title = "";
+  for (const item of Data) {
+    if (item.id === dataid) {
+      title = item.title;
+      content = item.content;
+      break;
+    }
+  }
+
+  return (
+    <>
+      {/* <div className="modalCard">
+        <div className="profile_Card" key={dataid}>
+          <div className="profile_2">
+            <div className="img">
+              <img src={imgsrc} alt="Profile of author" />
+            </div>
+          </div>
+          <div className="modalRight">
+            <div className="modalHead one">{head}</div>
+            <div className="authHead two">{name}</div>
+            <div className="content">{content}</div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="modalCard">
+        <div className={style.modalartist} key={dataid}>
+          <div className={style.ttle}>
+            <h1>{title}</h1>
+          </div>
+          <div className={style.contentmodal}>
+            <h3>{content}</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default UpcomingModal;
