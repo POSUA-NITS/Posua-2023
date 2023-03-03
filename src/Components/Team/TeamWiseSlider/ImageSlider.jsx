391b1b11c45ad59cd23731976000dd6881556efc
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-03-04 03:32:27 +0530

    Team Members updated

--- src/Components/Team/TeamWiseSlider/ImageSlider.jsx
+++ src/Components/Team/TeamWiseSlider/ImageSlider.jsx
@@ -39,6 +39,22 @@ const ImageSlider = ({ images }) => {
   return (
     <>
       <div className={styles.imgslider}>
+        <p className={styles.Designation}>Executive Heads</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
         <p className={styles.Designation}>Web Team</p>
         <div className={styles.sliderWrapper}>
           <Slider {...settings} className={styles.slide}>
@@ -56,6 +72,198 @@ const ImageSlider = ({ images }) => {
       </div>
       <div className={styles.imgslider}>
         <p className={styles.Designation}>Developers</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Marketing Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Decoration Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Event Management Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Hospitality Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Cultural Rally Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Infrastructure Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Food Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Logistics Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Design Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Content Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>T-Shirt Team</p>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} className={styles.slide}>
+            {MemberSlider &&
+              MemberSlider.map((item) => (
+                <div className={styles.sliderDiv} key={item.id}>
+                  <img className={styles.Card} src={item.card} />
+                  <p className={styles.Name}>{item.name}</p>
+                  <p className={styles.TextForRotation}>{item.text}</p>
+                  <img className={styles.Images} src={item.src} alt={item.alt} />
+                </div>
+              ))}
+          </Slider>
+        </div>
+      </div>
+      <div className={styles.imgslider}>
+        <p className={styles.Designation}>Design Team</p>
         <div className={styles.sliderWrapper}>
           <Slider {...settings} className={styles.slide}>
             {MemberSlider &&
