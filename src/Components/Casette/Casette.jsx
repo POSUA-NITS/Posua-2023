0d349fc4478a6496d43de885a5c730385d42c122
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-03-04 11:44:58 +0530

    FIXED: 2020

--- src/Components/Casette/Casette.jsx
+++ src/Components/Casette/Casette.jsx
@@ -17,8 +17,8 @@ const Casette = (props) => {
         </div>
         <div className={Styles.cassette_body}>
           <img src="https://res.cloudinary.com/dhgfsshq4/image/upload/v1677827002/posua2023/cassete_body_jg5yxf.webp" alt="cassette" className={Styles.body} />
-          <img src="https://res.cloudinary.com/dhgfsshq4/image/upload/v1677821241/posua2023/button_left_gkghbz.webp" alt="" onClick={()=>{setYear(parseInt(year)-1); setCdRotate(cdRotate - 180)}} className={`${Styles.button_left} ${year<=2016?Styles.disabled:''}`} />
-          <img src="https://res.cloudinary.com/dhgfsshq4/image/upload/v1677821241/posua2023/button_left_gkghbz.webp" alt="" onClick={()=>{setYear(parseInt(year)+1); setCdRotate(cdRotate + 180)}} className={`${Styles.button_right} ${year>=2022?Styles.disabled:''}`} />
+          <img src="https://res.cloudinary.com/dhgfsshq4/image/upload/v1677821241/posua2023/button_left_gkghbz.webp" alt="" onClick={()=>{year===2021?setYear(parseInt(year)-2):setYear(parseInt(year)-1); setCdRotate(cdRotate - 180)}} className={`${Styles.button_left} ${year<=2016?Styles.disabled:''}`} />
+          <img src="https://res.cloudinary.com/dhgfsshq4/image/upload/v1677821241/posua2023/button_left_gkghbz.webp" alt="" onClick={()=>{year===2019?setYear(parseInt(year)+2):setYear(parseInt(year)+1); setCdRotate(cdRotate + 180)}} className={`${Styles.button_right} ${year>=2022?Styles.disabled:''}`} />
         </div>
       </div>
     </div>
